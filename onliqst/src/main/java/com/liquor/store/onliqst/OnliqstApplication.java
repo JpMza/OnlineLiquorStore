7ea77213bfb216fe108c8676b19662666c03f357
Juan Pablo <jp_mendoza25@hotmail.com.ar>
2018-12-25 22:00:34 -0300

    Project Start

---
+++ onliqst/src/main/java/com/liquor/store/onliqst/OnliqstApplication.java
@@ -0,0 +1,14 @@
+package com.liquor.store.onliqst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnliqstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnliqstApplication.class, args);
+	}
+
+}
+
