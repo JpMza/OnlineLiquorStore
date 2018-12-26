cf13a2ce3a32e770469f0f6e03a8ca79c38c2345
JpMza <juanpablo.mendoza@possumus.com>
2018-12-25 23:30:00 -0300

    initial configurations

---
+++ onliqst/src/main/java/com/liquor/store/onliqst/config/PersistenceConfig.java
@@ -0,0 +1,7 @@
+package com.liquor.store.onliqst.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "com.liquor.store.onliqst.repositories")
+public class PersistenceConfig {
+}
---
+++ onliqst/src/main/java/com/liquor/store/onliqst/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.liquor.store.onliqst.repositories;
+
+import com.liquor.store.onliqst.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByName(String name);
+}
+
